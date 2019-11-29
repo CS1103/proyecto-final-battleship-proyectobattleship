6f8ad579351bb55ad110ddb222bdaf3273706e8e
Mauricio-Bernuy <54604331+Mauricio-Bernuy@users.noreply.github.com>
2019-11-29 17:09:13 -0500

    terminate on exception

--- battleship_CPU/handshakephase.h
+++ battleship_CPU/handshakephase.h
@@ -1,12 +1,13 @@
 #include "sendCommands.h"
 #include "checkOutputs.h"
 #include "parseScope.h"
+#include <exception>
 
 using path = std::filesystem::path;
 using namespace std;
 
-void handshake(int &token, int &ScopeX, int &ScopeY, string &user, string& username, path& input_directory, 
-				path& output_directory, path& temp_directory, int &checkcount){
+void handshake(int& token, int& ScopeX, int& ScopeY, string& user, string& username, path& input_directory,
+	path& output_directory, path& temp_directory, int& checkcount) {
 	string command, status, scope;
 	//step 1: send handshake command
 	sendCommand(0, "HANDSHAKE", username, input_directory, temp_directory);
@@ -14,11 +15,14 @@ void handshake(int &token, int &ScopeX, int &ScopeY, string &user, string& usern
 	//step2: wait and check output
 	_sleep(0.5);
 	checkOutput(command, status, token, scope, user, output_directory, checkcount);
-	if (command == "HANDSHAKE" && status == "ACCEPTED") {
-		cout << endl<<"token and scope accepted"<<endl;
-		parseScope(scope, ScopeX, ScopeY);
-		cout << "token: " << token<<endl;
-		cout << "dimensions: 0-" << ScopeX << ", 0-" << ScopeY << endl;
-
-	} else cout << "status rejected or command isn't handshake"<<endl;
+		if (command == "HANDSHAKE" && status == "ACCEPTED") {
+			cout << endl << "token and scope accepted" << endl;
+			parseScope(scope, ScopeX, ScopeY);
+			cout << "token: " << token << endl;
+			cout << "dimensions: 0-" << ScopeX << ", 0-" << ScopeY << endl;
+		}
+		else {
+			cout << "status rejected or command isn't handshake" << endl;
+			terminate();
+		}
 }
